c2392133909945c1ab57b2ee80dcc1a9f56b9c20
yueeong <yueeong@gmail.com>
2014-08-18 02:11:32 -0500

    problem 5 done

--- problem5.py
+++ problem5.py
@@ -1,6 +1,28 @@
 __author__ = 'yueeong'
 
+
+
+UPPERLIMIT = 20
+
+def tester(number):
+    #print "\ntesting " + str(number)
+    for i in range(1,UPPERLIMIT+1,1):
+        if number % i == 0:
+            #print str(number) + " is divisible by " +str(i)
+            if i == UPPERLIMIT:
+                print str(number) + " is IT!"
+                return True
+            else:
+                continue
+
+        elif number % i != 0:
+            #print str(number) + " not divisible by " +str(i) + " fail"
+            return False
+
 def calculate() :
-    largestnum = 1
-    for i in range(1,20,1):
-        if
+    largestnum = 2520
+    while tester(largestnum) != True:
+        largestnum +=2520
+
+
+calculate()
